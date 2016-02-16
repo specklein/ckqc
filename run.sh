1bd27da1a8c46f5d1a7837c76f350f6f4e1df23b
venkatv <venkatv@ip-10-22-0-206.ap-southeast-1.compute.internal>
2016-02-16 09:21:58 +0000

    Update run.sh as an executable

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-./vendor/bin/phpunit --no-globals-backup --bootstrap bootstrapQC.php --debug tests/   reportDate=20160118
+./vendor/bin/phpunit --no-globals-backup --bootstrap bootstrapQC.php --debug tests/  reportFile=/tmp/ck-daily-sales.csv reportDate=20160118 $$
 
