9a58f94dfb27fdb1463cf150759eb050c5546ceb
venkatv <venkatv@ip-10-22-0-206.ap-southeast-1.compute.internal>
2016-02-26 08:28:23 +0000

    Directed the logs to log file

--- tests/RevenueReportPaymentGatewayTest.php
+++ tests/RevenueReportPaymentGatewayTest.php
@@ -33,7 +33,7 @@ class RevenueReportPaymentGatewayTest extends BaseTestCase {
         throw new Exception("Not bootstrapped properly. Missing revenueOrdersModel");
       }
     }
-    self::$logger->debug("Revenue orders".PHP_EOL.print_r(self::$revenueOrdersModel->getOrders()));
+    self::$logger->debug("Revenue orders".PHP_EOL.print_r(self::$revenueOrdersModel->getOrders(),true));
     self::$logger->info("END ". __METHOD__);
 
   }
